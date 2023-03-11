08aa9a5d3358dd8158a36d143f0622cc030df210
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-03-11 15:00:40 -0300

    feat: add route api

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Request, Response } from 'express';
+import { CreateUserController } from './infra/controllers/create-user/create-user';
 import { GetUserController } from './infra/controllers/get-users/get-users';
 
 const app = express();
@@ -6,13 +7,15 @@ const app = express();
 app.use(express.json());
 
 app.get('/users', async (_req, res: Response) => {
-  const getusersController = new GetUserController();
-  const { statusCode, body } = await getusersController.execute();
+  const getUsersController = new GetUserController();
+  const { statusCode, body } = await getUsersController.execute();
   return res.status(statusCode).json({ users: body });
 });
 
 app.post('/users', async (req: Request, res: Response) => {
-  return res.status(200).json({ data: req.body })
+  const createUsers = new CreateUserController();
+  const { statusCode, body } = await createUsers.execute({ body: req.body });
+  res.status(statusCode).json(body);
 });
 
 export default app;
