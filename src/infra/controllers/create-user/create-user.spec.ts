3fe2d4f180e15ff1f03cf96702281513164f36a9
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-03-11 15:00:24 -0300

    feat: test create user

---
+++ src/infra/controllers/create-user/create-user.spec.ts
@@ -0,0 +1,36 @@
+import { InMemoryCreateUserRepository } from 'test/repositories/in-memory-user-repository';
+import { expect, test } from 'vitest';
+import { CreateUserController } from './create-user';
+import { CreateUserProps } from './protocols';
+
+test('create user', async () => {
+  const inMemoryCreateUserRepository = new InMemoryCreateUserRepository();
+  const createUserController = new CreateUserController(
+    inMemoryCreateUserRepository
+  );
+
+  const user: CreateUserProps = {
+    name: 'user test',
+    email: 'test@test.com',
+    adress: 'brazil',
+  };
+
+  const { statusCode, body } = await createUserController.execute({
+    body: user,
+  });
+
+  expect(statusCode).toEqual(201);
+  expect(body).toContain({ name: 'user test' });
+});
+
+test('no create user without props', async () => {
+  const inMemoryCreateUserRepository = new InMemoryCreateUserRepository();
+  const createUserController = new CreateUserController(
+    inMemoryCreateUserRepository
+  );
+
+  const { statusCode, body } = await createUserController.execute({});
+
+  expect(statusCode).toEqual(400);
+  expect(body).toEqual('Please specify a body');
+});
--- test/repositories/in-memory-user-repository.ts
+++ test/repositories/in-memory-user-repository.ts
@@ -1,4 +1,8 @@
 import { User } from '~/appplication/interfaces/user';
+import {
+  CreateUserProps,
+  ICreateUserRepository,
+} from '~/infra/controllers/create-user/protocols';
 import { IGetUsersRepository } from '~/infra/controllers/get-users/protocols';
 
 export class InMemoryGetUsersRepository implements IGetUsersRepository {
@@ -7,26 +11,43 @@ export class InMemoryGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
     return this.users;
   }
-
-  // async findById(userId: string): Promise<User | null> {
-  //   const user = this.users.find((user) => user.value.id === userId);
-
-  //   if (!user) return null;
-
-  //   return user;
-  // }
-
-  // async create(user: User): Promise<void> {
-  //   this.users.push(user);
-  // }
-
-  // async save(user: User): Promise<void> {
-  //   const userIndex = this.users.findIndex(
-  //     (_user) => _user.value.id === user.value.id
-  //   );
-
-  //   if (userIndex >= 0) {
-  //     this.users[userIndex] = user;
-  //   }
-  // }
 }
+
+export class InMemoryCreateUserRepository implements ICreateUserRepository {
+  public users: User[] = [];
+
+  async createUser(user: CreateUserProps): Promise<User> {
+    const newUser: User = {
+      ...user,
+      bag: {
+        items: [],
+      },
+    };
+
+    this.users.push(newUser);
+
+    return newUser;
+  }
+}
+
+// async findById(userId: string): Promise<User | null> {
+//   const user = this.users.find((user) => user.value.id === userId);
+
+//   if (!user) return null;
+
+//   return user;
+// }
+
+// async create(user: User): Promise<void> {
+//   this.users.push(user);
+// }
+
+// async save(user: User): Promise<void> {
+//   const userIndex = this.users.findIndex(
+//     (_user) => _user.value.id === user.value.id
+//   );
+
+//   if (userIndex >= 0) {
+//     this.users[userIndex] = user;
+//   }
+// }
