e9ceaf0b4478c694c87613032aff8365a6258d8e
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-03-12 20:12:56 -0300

    feat: add mongo respository to add item in bag

---
+++ src/infra/repositories/add-item-bag/mongo-add-item-bag.ts
@@ -0,0 +1,35 @@
+import { UserODM } from '~/appplication/models/user-odm';
+import {
+  AddItemToBagProps,
+  IAddItemToBagRepository,
+} from '~/infra/controllers/add-item-bag/protocols';
+import { AddItemToBagResponse } from '../../controllers/add-item-bag/protocols';
+
+export class MongoAddItemBagRepository implements IAddItemToBagRepository {
+  constructor(private _model: UserODM = new UserODM()) {}
+
+  async addItemToBag(
+    userId: string,
+    props: AddItemToBagProps
+  ): Promise<AddItemToBagResponse> {
+    try {
+      const userWithBag = await this._model.getById(userId);
+
+      if (!userWithBag) {
+        throw new Error('Missing user id');
+      }
+      
+      userWithBag.bag.push(props);
+  
+      const userUpdated = await this._model.updateById(userId, userWithBag);
+  
+      if (!userUpdated) {
+        throw new Error('Internal Error');
+      }
+  
+      return { bag: userUpdated.bag };
+    } catch {
+      throw new Error('Internal Error')
+    }
+  }
+}
