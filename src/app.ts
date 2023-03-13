cd0f1cc2831adaba1d17d02ed50af90ed4fcbfa5
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-03-13 19:01:13 -0300

    refactor: add router for users with Router of express

--- src/app.ts
+++ src/app.ts
@@ -1,53 +1,10 @@
-import express, { Request, Response } from 'express';
-import { AddItemToBagController } from './infra/controllers/add-item-bag/add-item-bag';
-import { CreateUserController } from './infra/controllers/user/create-user/create-user';
-import { DeleteUserController } from './infra/controllers/user/delete-user/delete-user';
-import { GetUserController } from './infra/controllers/user/get-users/get-users';
-import { UpdateUserController } from './infra/controllers/user/update-user/update-user';
+import express from 'express';
+import usersRouter from './infra/routes/users';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/users', async (_req, res: Response) => {
-  const getUsersController = new GetUserController();
-  const { statusCode, body } = await getUsersController.execute();
-  return res.status(statusCode).json({ users: body });
-});
-
-app.post('/users', async (req: Request, res: Response) => {
-  const createUsers = new CreateUserController();
-  const { statusCode, body } = await createUsers.execute({ body: req.body });
-  res.status(statusCode).json(body);
-});
-
-app.patch('/users/:id', async (req: Request, res: Response) => {
-  const updateUserController = new UpdateUserController();
-  const { statusCode, body } = await updateUserController.execute({
-    body: req.body,
-    params: req.params,
-  });
-
-  return res.status(statusCode).json(body);
-});
-
-app.delete('/users/:id', async (req: Request, res: Response) => {
-  const deleteUserController = new DeleteUserController();
-  const { statusCode, body } = await deleteUserController.execute({
-    params: req.params,
-  });
-
-  return res.status(statusCode).json(body);
-});
-
-app.patch('/users/add-item-bag/:id', async (req: Request, res: Response) => {
-  const addItemToBagController = new AddItemToBagController();
-  const { statusCode, body } = await addItemToBagController.execute({
-    body: req.body,
-    params: req.params,
-  });
-
-  return res.status(statusCode).json(body);
-});
+app.use('/users', usersRouter);
 
 export default app;
---
+++ src/infra/routes/users.ts
@@ -0,0 +1,51 @@
+import { Request, Response, Router } from 'express';
+import { AddItemToBagController } from '../controllers/user/add-item-bag/add-item-bag';
+import { CreateUserController } from '../controllers/user/create-user/create-user';
+import { DeleteUserController } from '../controllers/user/delete-user/delete-user';
+import { GetUserController } from '../controllers/user/get-users/get-users';
+import { UpdateUserController } from '../controllers/user/update-user/update-user';
+
+const usersRouter = Router();
+
+usersRouter.get('/', async (_req: Request, res: Response) => {
+  const getUsersController = new GetUserController();
+  const { statusCode, body } = await getUsersController.execute();
+  return res.status(statusCode).json({ users: body });
+});
+
+usersRouter.post('/', async (req: Request, res: Response) => {
+  const createUsers = new CreateUserController();
+  const { statusCode, body } = await createUsers.execute({ body: req.body });
+  res.status(statusCode).json(body);
+});
+
+usersRouter.patch('/:id', async (req: Request, res: Response) => {
+  const updateUserController = new UpdateUserController();
+  const { statusCode, body } = await updateUserController.execute({
+    body: req.body,
+    params: req.params,
+  });
+
+  return res.status(statusCode).json(body);
+});
+
+usersRouter.delete('/:id', async (req: Request, res: Response) => {
+  const deleteUserController = new DeleteUserController();
+  const { statusCode, body } = await deleteUserController.execute({
+    params: req.params,
+  });
+
+  return res.status(statusCode).json(body);
+});
+
+usersRouter.patch('/add-item-bag/:id', async (req: Request, res: Response) => {
+  const addItemToBagController = new AddItemToBagController();
+  const { statusCode, body } = await addItemToBagController.execute({
+    body: req.body,
+    params: req.params,
+  });
+
+  return res.status(statusCode).json(body);
+});
+
+export default usersRouter;
