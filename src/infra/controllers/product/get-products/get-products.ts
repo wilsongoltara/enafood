d42d86e4ae4e750a23c5b4c0248a484fa4b66e20
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-03-13 19:26:26 -0300

    feat: add GetProductController and its router

---
+++ src/infra/controllers/product/get-products/get-products.ts
@@ -0,0 +1,21 @@
+import { Product } from '~/appplication/interfaces/product';
+import { MongoGetProductsRepository } from '~/infra/repositories/product/get-products/mongo-get-products';
+import { internalError, ok } from '../../helpers';
+import { HttpResponse, IController } from '../../protocols';
+import { IGetProductsRepository } from './protocols';
+
+export class GetProductsController implements IController {
+  constructor(
+    private readonly getProductsRepository: IGetProductsRepository = new MongoGetProductsRepository()
+  ) {}
+
+  async execute(): Promise<HttpResponse<Product[] | string>> {
+    try {
+      const products = await this.getProductsRepository.getProducts();
+
+      return ok<Product[]>(products);
+    } catch {
+      return internalError();
+    }
+  }
+}
--- src/infra/routes/products.ts
+++ src/infra/routes/products.ts
@@ -1,13 +1,21 @@
 import { Request, Response, Router } from 'express';
+import { GetProductsController } from '../controllers/product/get-products/get-products';
 import { RegisterProductController } from '../controllers/product/register-product/resgister-product';
 
 const productsRouter = Router();
+
+productsRouter.get('/', async (req: Request, res: Response) => {
+  const getProductsConroller = new GetProductsController();
+  const { statusCode, body } = await getProductsConroller.execute();
+
+  return res.status(statusCode).json(body);
+});
 
 productsRouter.post('/', async (req: Request, res: Response) => {
   const registerProductController = new RegisterProductController();
   const { statusCode, body } = await registerProductController.execute({
     params: req.params,
-    body: req.body
+    body: req.body,
   });
 
   return res.status(statusCode).json(body);
