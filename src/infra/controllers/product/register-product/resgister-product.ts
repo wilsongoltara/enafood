baaf90419fc9bc705d5886d3f6bdd97f3559889a
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-03-13 19:10:44 -0300

    feat: add registerProductController

---
+++ src/infra/controllers/product/register-product/resgister-product.ts
@@ -0,0 +1,33 @@
+import { Product } from '~/appplication/interfaces/product';
+import { MongoRegisterProductRepository } from '~/infra/repositories/product/register-product/mongo-register-product';
+import { badRequest, created, internalError } from '../../helpers';
+import { HttpRequest, HttpResponse, IController } from '../../protocols';
+import { IRegisterProductRepository, RegisterProductProps } from './protocols';
+
+export class RegisterProductController implements IController {
+  constructor(
+    private readonly registerProductRepository: IRegisterProductRepository = new MongoRegisterProductRepository()
+  ) {}
+
+  async execute(
+    httpRequest: HttpRequest<RegisterProductProps>
+  ): Promise<HttpResponse<Product | string>> {
+    try {
+      const requiredFields = ['name', 'description', 'price'];
+
+      for (const field of requiredFields) {
+        if (!httpRequest?.body?.[field as keyof RegisterProductProps]) {
+          return badRequest(`Field ${field} is required`);
+        }
+      }
+
+      const userAdded = await this.registerProductRepository.registerProduct(
+        httpRequest.body!
+      );
+
+      return created<Product>(userAdded);
+    } catch {
+      return internalError();
+    }
+  }
+}
