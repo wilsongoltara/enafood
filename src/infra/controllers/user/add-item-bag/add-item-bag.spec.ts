dfe4111368548d93c6f6e6e7c36c814d85880bd6
Wilson S. Goltara <wilson.goltara15@gmail.com>
2023-03-20 14:42:43 -0300

    refactor: errors after modify user interface

--- src/infra/controllers/user/add-item-bag/add-item-bag.spec.ts
+++ src/infra/controllers/user/add-item-bag/add-item-bag.spec.ts
@@ -1,31 +1,33 @@
-import { InMemoryAddItemToBagRepository, InMemoryUpdateUserRepository } from "test/repositories/in-memory-user-repository";
-import { expect, test } from "vitest";
-import { User } from "~/appplication/interfaces/user";
-import { AddItemToBagProps } from "./protocols";
-import { AddItemToBagController } from "./add-item-bag";
+import { InMemoryAddItemToBagRepository } from 'test/repositories/in-memory-user-repository';
+import { expect, test } from 'vitest';
+import { User } from '~/appplication/interfaces/user';
+import { AddItemToBagController } from './add-item-bag';
+import { AddItemToBagProps } from './protocols';
 
 test('add item in bag', async () => {
   const inMemoryAddItemToBagRepository = new InMemoryAddItemToBagRepository();
-  const addItemToBagController = new AddItemToBagController(inMemoryAddItemToBagRepository);
+  const addItemToBagController = new AddItemToBagController(
+    inMemoryAddItemToBagRepository
+  );
 
   const user: User = {
     id: '640e581823c41b27aa35599c',
     name: 'user test',
     email: 'test@test.com',
     adress: 'brazil',
-    bag: []
+    bag: [],
   };
 
   inMemoryAddItemToBagRepository.users.push(user);
 
   const item1: AddItemToBagProps = {
-    item: 'Coca-cola',
+    productId: 'productId-1',
     quantity: 2,
-  }
+  };
 
   const { statusCode, body } = await addItemToBagController.execute({
     params: { id: user.id },
-    body: item1
+    body: item1,
   });
 
   expect(statusCode).toEqual(200);
@@ -34,26 +36,28 @@ test('add item in bag', async () => {
 
 test('No add item without user id', async () => {
   const inMemoryAddItemToBagRepository = new InMemoryAddItemToBagRepository();
-  const addItemToBagController = new AddItemToBagController(inMemoryAddItemToBagRepository);
+  const addItemToBagController = new AddItemToBagController(
+    inMemoryAddItemToBagRepository
+  );
 
   const user: User = {
     id: '640e581823c41b27aa35599c',
     name: 'user test',
     email: 'test@test.com',
     adress: 'brazil',
-    bag: []
+    bag: [],
   };
 
   inMemoryAddItemToBagRepository.users.push(user);
 
   const item1: AddItemToBagProps = {
-    item: 'Coca-cola',
+    productId: 'productId-1',
     quantity: 2,
-  }
+  };
 
   const { statusCode, body } = await addItemToBagController.execute({
-    params: { },
-    body: item1
+    params: {},
+    body: item1,
   });
 
   expect(statusCode).toEqual(400);
@@ -62,27 +66,30 @@ test('No add item without user id', async () => {
 
 test('No add item with invalid props', async () => {
   const inMemoryAddItemToBagRepository = new InMemoryAddItemToBagRepository();
-  const addItemToBagController = new AddItemToBagController(inMemoryAddItemToBagRepository);
+  const addItemToBagController = new AddItemToBagController(
+    inMemoryAddItemToBagRepository
+  );
 
   const user: User = {
     id: '640e581823c41b27aa35599c',
     name: 'user test',
     email: 'test@test.com',
     adress: 'brazil',
-    bag: []
+    bag: [],
   };
 
   inMemoryAddItemToBagRepository.users.push(user);
 
   const item1 = {
-    item: 'Coca-cola',
+    product: 'productId-1',
   };
 
+  // props with invalid key
   const { statusCode, body } = await addItemToBagController.execute({
     params: { id: user.id },
-    body: item1
+    body: item1,
   });
 
   expect(statusCode).toEqual(400);
-  expect(body).toEqual('Field quantity is required');
+  expect(body).toEqual('Field productId is required');
 });
